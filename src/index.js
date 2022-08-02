16098ad6bffc445216b56a2e892f3da85778809d
Alex Moskovskyi <mscw81@gmail.com>
2022-08-02 22:31:33 +0300

    feat: add function sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
-// You should implement your task here.
-
+function towelSort(arr){
+arrsort = arr.join().split(',').sort(function(a, b) { return a - b }),
+newarr = [];
+for(var i = 0; i < arrsort.length; i++){
+  newarr[i]=parseInt(arrsort[i]);
+  }
+  return newarr
+}
 module.exports = function towelSort (matrix) {
   return [];
 }
